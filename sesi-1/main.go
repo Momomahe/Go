b1fbb6b12fd490bd0538e6c9ff2d8d16fc1a457a
Momomahe <127234239+Momomahe@users.noreply.github.com>
2023-03-09 23:36:48 +0700

    Add files via upload

--- sesi-1/main.go
+++ sesi-1/main.go
@@ -6,20 +6,20 @@ func main() {
 	i := 21
 	j := true
 	k := 123.456
+	kode := '\u042F'
 	russia := 'Я'
-
 	fmt.Printf("%v\n", i)
 	fmt.Printf("%T\n", i)
 	fmt.Printf("%%\n")
 	fmt.Printf("%t\n", j)
-	fmt.Printf("%b\n", i)      // menampilkan hasil biner dari "i"
+	fmt.Printf("\n%b\n", i) // menampilkan hasil biner dari "i"
+	fmt.Printf("%c\n", kode)
 	fmt.Printf("%d\n", i)      // menampilkan hasil desimal dari "i"
 	fmt.Printf("%o\n", i)      // menampilkan hasil oktal dari "i"
 	fmt.Printf("%x\n", 15)     // untuk mendapatkan hex dgn kode f perlu mencari angka yang cocok
 	fmt.Printf("%X\n", 15)     // untuk mendapatkan hex dgn kode F perlu mencari angka yang cocok
 	fmt.Printf("%U\n", russia) // %U (menampilkan unicode)
-	fmt.Printf("%.3f\n", k)
-	fmt.Printf("%.6f\n", k)
+	fmt.Printf("\n%.6f\n", k)
 	fmt.Printf("%E\n", k)
 
 	/* beda f dan F hanya terletak pada format outpu "%x"(utk huruf kecil)
