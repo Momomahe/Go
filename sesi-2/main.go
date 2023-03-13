882adf7b33a60a9a8560b8212463f7a6c7a84a59
Momomahe <127234239+Momomahe@users.noreply.github.com>
2023-03-13 22:49:11 +0700

    Update main.go

--- sesi-2/main.go
+++ sesi-2/main.go
@@ -10,9 +10,14 @@ func main() {
 	}
 	for j := 0; j < 11; j++ {
 		if j == 5 {
-			r := []rune{'\u0421', '\u0410', '\u0428', '\u0410', '\u0420', '\u0412', '\u041E'}
+			r := "САШАРВО"
+			//r := []rune{'\u0421', '\u0410', '\u0428', '\u0410', '\u0420', '\u0412', '\u041E'}
+			/*
+			utk variabel r jg bsa digunakan, akan ttpi variabel posisi dibawah dikalikan dngn 2
+			*/
 			for posisi, loop := range r {
-				fmt.Printf("Character %U '%c' starts at byte position %d\n", loop, loop, posisi*2)
+				fmt.Printf("Character %U '%c' starts at byte position %d\n", loop, loop, posisi)
+				//fmt.Printf("Character %U '%c' starts at byte position %d\n", loop, loop, posisi*2)
 			}
 			continue
 		}
